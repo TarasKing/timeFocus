e2a3225264d658153eec15fa06fd2ab06ce1c909
TarasKing <ivanovtaras@live.ru>
2023-03-31 01:03:35 -0700

    added Timer/Pause functionality

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Countdown } from "../comonents/Countdown";
+import { spacing, fontSizes } from "../utils/sizes";
+import { RoundedButton } from "../comonents/RoundedButton";
+
 export const Timer = ({ focusSubject, onTimerEnd, clearSubject }) => {
+  const [isStarted, setIsStarted] = useState(false);
+
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
-        <Countdown onProgress={() => {}} isPaused onEnd={() => {}} />
+        <Countdown
+          onProgress={() => {}}
+          isPaused={!isStarted}
+          onEnd={() => {}}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        {!isStarted ? (
+          <RoundedButton title="start" onPress={() => setIsStarted(true)} />
+        ) : (
+          <RoundedButton title="pause" onPress={() => setIsStarted(false)} />
+        )}
       </View>
     </View>
   );
@@ -20,5 +36,14 @@ const styles = StyleSheet.create({
     flex: 0.5,
     alignItems: "center",
     backgroundColor: "blue",
+    justifyContent: "center",
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    flexDirection: "row",
+    padding: spacing.md,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "yellow",
   },
 });
